dad30c6669e1344adde814719515872f2cc50e91
prakashmul <100404694+prakashmul@users.noreply.github.com>
2024-08-23 17:28:15 +0545

    middleware

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>{loading? "Processing" : "Login"}</h1>
+            <h1>{loading ? "Processing" : "Login"}</h1>
             <hr />
 
 
